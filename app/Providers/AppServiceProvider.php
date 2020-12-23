e87be95816db4fdf1cc6423c4f28fec3d011094a
jeespoping <jeespoping@gmail.com>
2020-12-22 19:39:00 -0500

    configurando las carpetas Schemas, Adapters y Validators

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 
+use CloudCreativity\LaravelJsonApi\LaravelJsonApi;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        LaravelJsonApi::defaultApi('v1');
     }
 }
